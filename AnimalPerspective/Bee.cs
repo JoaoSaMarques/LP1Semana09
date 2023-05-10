f04a7de4d61ac1d7dfe9585caf31d675b999116f
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-10 14:19:22 +0100

    get num of wings

--- AnimalPerspective/Bee.cs
+++ AnimalPerspective/Bee.cs
@@ -4,9 +4,16 @@ namespace OverrideVsNew
 {
     public class Bee : Animal, ICanFly
     {
+        //Number of wings
+        public int NumberOfWings { get; }
+
+        /// <summary>
+        /// Sound it makes
+        /// </summary>
+        /// <returns></returns>
         public override string Sound()
         {
-            return base.Sound() + "Woof!";
+            return base.Sound() + "bzzzz!";
         }
     }
 }
