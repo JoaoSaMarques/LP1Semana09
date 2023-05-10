5dd3e390353d2525795895fd2398eb9e263141b8
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-10 14:46:51 +0100

    Num of nipples for dog and cat

--- AnimalPerspective/Cat.cs
+++ AnimalPerspective/Cat.cs
@@ -4,6 +4,16 @@ namespace AnimalPerspective
 {
     public class Cat : Animal, IMammal
     {
+        public int NumberOfNipples { get; }
+
+        /// <summary>
+        /// Constructor of dog
+        /// </summary>
+        public Cat()
+        {
+            NumberOfNipples = 10
+        }
+
         public override string Sound()
         {
             return base.Sound() + "Miau";
--- AnimalPerspective/Dog.cs
+++ AnimalPerspective/Dog.cs
@@ -4,6 +4,16 @@ namespace AnimalPerspective
 {
     public class Dog : Animal, IMammal
     {
+        public int NumberOfNipples { get; }
+
+        /// <summary>
+        /// Constructor of dog
+        /// </summary>
+        public Dog()
+        {
+            NumberOfNipples = 10
+        }
+        
         public override string Sound()
         {
             return base.Sound() + "Woof!";
