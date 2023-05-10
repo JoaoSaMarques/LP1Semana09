2b428f01026bb7b33a67a4a095273c889a66a63a
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-10 16:06:19 +0100

    Instance

--- PlayerManager1/Program.cs
+++ PlayerManager1/Program.cs
@@ -19,5 +19,17 @@ namespace PlayerManager1
             Program prog = new Program();
             prog.Start();
         }
+
+        /// <summary>
+        /// Creates a new instance of the player listing program.
+        /// </summary>
+        private Program()
+        {
+            playerList = new List<Player>() 
+            {
+                new Player("Best player ever", 100),
+                new Player("An even better player", 500)
+            };
+        }
     }
 }
