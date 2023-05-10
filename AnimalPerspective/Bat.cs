9064c4fc1d04840fc722615144bfadecdaa31db9
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-10 14:16:34 +0100

    NumofNipples and Wings

--- AnimalPerspective/Bat.cs
+++ AnimalPerspective/Bat.cs
@@ -4,6 +4,14 @@ namespace AnimalPerspective
 {
     public class Bat : Animal, IMammal, ICanFly
     {
+        //Number of nipples on the animal
+        public int NumberOfNipples { get; }
+
+        //How many wings does it have
+        public int NumberOfWings { get; }
+
+
+        //Play sound
         public override string Sound()
         {
             return base.Sound() + "Woof!";
