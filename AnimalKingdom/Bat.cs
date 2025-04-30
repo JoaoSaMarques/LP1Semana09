2fd6f83ffb912c65b12d50d163907cef062ae74a
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-04-30 14:11:36 +0100

    update cat

---
+++ AnimalKingdom/Bat.cs
@@ -0,0 +1,13 @@
+namespace AnimalKingdom
+{
+    public class Bat : Animal, IMammal, ICanFly
+    {
+        public int NumberOfNipples { get; } = 2;
+        public int NumberOfWings { get; } = 2;
+
+        public override string Sound()
+        {
+            return base.Sound() + "Screech";
+        }
+    }
+}
