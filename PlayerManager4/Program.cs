6a8d4e75a2ab1a732b31fe0791ae209709ddd028
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-04-30 14:42:25 +0100

    update Program

--- PlayerManager4/Program.cs
+++ PlayerManager4/Program.cs
@@ -4,56 +4,33 @@ using System.Linq;
 
 namespace PlayerManager4
 {
-    /// <summary>
-    /// The player listing program.
-    /// </summary>
     public class Program
     {
-        /// <summary>
-        /// The list of all players.
-        /// </summary>
         private List<Player> playerList;
 
-        /// <summary>
-        /// Program begins here.
-        /// </summary>
         private static void Main()
         {
-            // Create a new instance of the player listing program
             Program prog = new Program();
-            // Start the program instance
             prog.Start();
         }
 
-        /// <summary>
-        /// Creates a new instance of the player listing program.
-        /// </summary>
         private Program()
         {
-            // Initialize the player list with two players using collection
-            // initialization syntax
             playerList = new List<Player>() {
                 new Player("Best player ever", 100),
                 new Player("An even better player", 500)
             };
         }
 
-        /// <summary>
-        /// Start the player listing program instance
-        /// </summary>
         private void Start()
         {
-            // We keep the user's option here
             string option;
 
-            // Main program loop
             do
             {
-                // Show menu and get user option
                 ShowMenu();
                 option = Console.ReadLine();
 
-                // Determine the option specified by the user and act on it
                 switch (option)
                 {
                     case "1":
@@ -66,6 +43,12 @@ namespace PlayerManager4
                         ListPlayersWithScoreGreaterThan();
                         break;
                     case "4":
+                        ListPlayersSortedByName(true);
+                        break;
+                    case "5":
+                        ListPlayersSortedByName(false);
+                        break;
+                    case "6":
                         Console.WriteLine("Bye!");
                         break;
                     default:
@@ -73,31 +56,25 @@ namespace PlayerManager4
                         break;
                 }
 
-                // Wait for user to press a key...
                 Console.Write("\nPress any key to continue...");
                 Console.ReadKey(true);
                 Console.WriteLine("\n");
 
-                // Loop keeps going until players choses to quit (option 4)
-            } while (option != "4");
+            } while (option != "6");
         }
 
-        /// <summary>
-        /// Shows the main menu.
-        /// </summary>
         private void ShowMenu()
         {
             Console.WriteLine("Player Manager Menu:");
             Console.WriteLine("1. Insert player");
-            Console.WriteLine("2. List all players");
+            Console.WriteLine("2. List all players (by score descending)");
             Console.WriteLine("3. List players with score greater than");
-            Console.WriteLine("4. Exit");
+            Console.WriteLine("4. List players by name ascending");
+            Console.WriteLine("5. List players by name descending");
+            Console.WriteLine("6. Exit");
             Console.Write("Enter your choice: ");
         }
 
-        /// <summary>
-        /// Inserts a new player in the player list.
-        /// </summary>
         private void InsertPlayer()
         {
             Console.Write("Enter player name: ");
@@ -120,15 +97,6 @@ namespace PlayerManager4
             Console.WriteLine($"Player '{name}' with score {score} added.");
         }
 
-        /// <summary>
-        /// Show all players in a list of players. This method can be static
-        /// because it doesn't depend on anything associated with an instance
-        /// of the program. Namely, the list of players is given as a parameter
-        /// to this method.
-        /// </summary>
-        /// <param name="playersToList">
-        /// An enumerable object of players to show.
-        /// </param>
         private static void ListPlayers(IEnumerable<Player> playersToList)
         {
             Console.WriteLine("\nList of players:");
@@ -138,9 +106,6 @@ namespace PlayerManager4
             }
         }
 
-        /// <summary>
-        /// Show all players with a score higher than a user-specified value.
-        /// </summary>
         private void ListPlayersWithScoreGreaterThan()
         {
             int minScore;
@@ -159,13 +124,6 @@ namespace PlayerManager4
             ListPlayers(filteredPlayers.OrderByDescending(p => p.Score));
         }
 
-        /// <summary>
-        /// Get players with a score higher than a given value.
-        /// </summary>
-        /// <param name="minScore">Minimum score players should have.</param>
-        /// <returns>
-        /// An enumerable of players with a score higher than the given value.
-        /// </returns>
         private IEnumerable<Player> GetPlayersWithScoreGreaterThan(int minScore)
         {
             foreach (var player in playerList)
@@ -175,6 +133,14 @@ namespace PlayerManager4
                     yield return player;
                 }
             }
+        }
+
+        private void ListPlayersSortedByName(bool ascending)
+        {
+            var comparer = new CompareByName(ascending);
+            var sortedList = new List<Player>(playerList);
+            sortedList.Sort(comparer);
+            ListPlayers(sortedList);
         }
     }
 }
